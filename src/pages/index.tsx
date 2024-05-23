d1d53e9d9d6ee9866371d106b6f8f8573104020a
vo huu nhan <vohuunhan1310@gmail.com>
2024-05-23 09:17:10 +0700

    feat: create account success

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import Home from '@/components/Home';
+import CreateWallet from '@/components/Wallet/CreateWallet';
+import CreateWalletSucess from '@/components/Wallet/CreateWalletSuccess';
+import CreateWalletv1 from '@/components/Wallet/CreateWalletv1';
 
 
 export default function IndexPage() {
   return (
-    <Home/>
+    <CreateWalletSucess/>
   );
 };
